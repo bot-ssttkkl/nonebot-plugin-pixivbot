89821df19693ec8dca7e0941a9fa863993d6624b
ssttkkl <huang.wen.long@hotmail.com>
2022-05-16 22:40:32 +0800

    使用numpy做随机抽选

---
+++ controller/roulette.py
@@ -0,0 +1,81 @@
+import math
+import numpy as np
+
+from time import time
+
+from ..data_source import LazyIllust
+
+
+def uniform(illusts: list[LazyIllust]) -> np.ndarray:
+    # 概率相等
+    n = len(illusts)
+    return np.ones(n) / n
+
+
+def bookmark_proportion(illusts: list[LazyIllust]) -> np.ndarray:
+    # 概率正比于书签数
+    n = len(illusts)
+    p = [0] * n
+
+    for i, x in enumerate(illusts):
+        if x.loaded:
+            p[i] = x.content.total_bookmarks + 10  # 加10平滑
+        else:
+            p[i] = 0
+
+    p = np.array(p)
+    return p / np.sum(p)
+
+
+def view_proportion(illusts: list[LazyIllust]) -> np.ndarray:
+    # 概率正比于查看人数
+    n = len(illusts)
+    p = [0] * n
+
+    for i, x in enumerate(illusts):
+        if x.loaded:
+            p[i] = x.content.total_view + 10  # 加10平滑
+        else:
+            p[i] = 0
+
+    p = np.array(p)
+    return p / np.sum(p)
+
+
+def timedelta_proportion(illusts: list[LazyIllust]) -> np.ndarray:
+    # 概率正比于 exp(归一化后的画像发布时间差)
+    n = len(illusts)
+    p = [0] * n
+
+    now = time()
+    for i, x in enumerate(illusts):
+        if x.loaded:
+            p[i] = x.create_date.timestamp() - now
+        else:
+            p[i] = float("-inf")
+
+    p = np.array(p)
+    p = p / (-np.min(p))  # 归一化
+    p = np.exp(p)
+    return p / np.sum(p)
+
+
+p_gen = {
+    "uniform": uniform,
+    "bookmark_proportion": bookmark_proportion,
+    "view_proportion": view_proportion,
+    "timedelta_proportion": timedelta_proportion,
+}
+
+
+def roulette(illusts: list[LazyIllust], random_method: str, k: int) -> list[LazyIllust]:
+    if random_method not in p_gen:
+        raise ValueError(f"illegal random_method: {random_method}")
+
+    n = len(illusts)
+    p = p_gen[random_method](illusts)
+    # print([(illusts[i].content.create_date, p[i]) for i in range(n)])
+
+    rng = np.random.default_rng()
+    winners = rng.choice(n, k, False, p)
+    return [illusts[c] for c in winners]
--- controller/service.py
+++ controller/service.py
@@ -10,6 +10,7 @@ from ..data_source import PixivBindings, PixivDataSource, LazyIllust
 from ..model import Illust, User
 from ..errors import BadRequestError, QueryError
 from .pkg_context import context
+from .roulette import roulette
 
 
 @context.export_singleton()
@@ -24,49 +25,9 @@ class Service:
         if count > self.conf.pixiv_max_item_per_query:
             raise BadRequestError("数量超过单次请求上限")
 
-        if random_method == "uniform":
-            # 概率相等
-            if len(illusts) == 0:
-                raise QueryError("别看了，没有的。")
-            probability = [1 / len(illusts)] * len(illusts)
-        else:
-            illusts = list(filter(lambda x: x.loaded, illusts))  # 只在已加载的插画中选择
-            if len(illusts) == 0:
-                raise QueryError("别看了，没有的。")
-
-            if random_method == "bookmark_proportion":
-                # 概率正比于书签数
-                sum_bm = 0
-                for x in illusts:
-                    sum_bm += x.total_bookmarks + 10  # 加10平滑
-
-                probability = [0] * len(illusts)
-                for i, x in enumerate(illusts):
-                    probability[i] = (x.total_bookmarks + 10) / sum_bm
-            elif random_method == "view_proportion":
-                # 概率正比于查看人数
-                sum_view = 0
-                for x in illusts:
-                    sum_view += x.total_view + 10  # 加10平滑
-
-                probability = [0] * len(illusts)
-                for i, x in enumerate(illusts):
-                    probability[i] = (x.total_view + 10) / sum_view
-            elif random_method == "timedelta_proportion":
-                # 概率正比于 exp((当前时间戳 - 画像发布时间戳) / 3e7)
-                now = time.time()
-                delta_time = [now - x.create_date.timestamp() for x in illusts]
-                probability = [math.exp(-x * 3e-7) for x in delta_time]
-                sum_poss = sum(probability)
-                for i in range(len(probability)):
-                    probability[i] = probability[i] / sum_poss
-            else:
-                raise ValueError(
-                    f"illegal random_method value: {random_method}")
-
-        choices = random.choices(illusts, probability, k=count)
-        logger.info(f"choice {[x.id for x in choices]}")
-        return [await x.get() for x in choices]
+        winners = roulette(illusts, random_method, count)
+        logger.info(f"choice {[x.id for x in winners]}")
+        return [await x.get() for x in winners]
 
     async def illust_ranking(self, mode: str,
                              range: typing.Union[typing.Sequence[int], int]) -> typing.List[Illust]:
