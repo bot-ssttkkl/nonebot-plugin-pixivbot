42bedcad5980abc069c1df8f1dd157ca30f29051
ssttkkl <huang.wen.long@hotmail.com>
2022-07-16 22:05:16 +0800

    IllustMessagesModel添加flat函数

--- src/nonebot_plugin_pixivbot/postman/model/illust_messages.py
+++ src/nonebot_plugin_pixivbot/postman/model/illust_messages.py
@@ -9,5 +9,10 @@ class IllustMessagesModel(BaseModel):
     header: Optional[str] = None
     messages: Sequence[IllustMessageModel]
 
+    def flat(self):
+        for x in self.messages:
+            yield x.copy(update={"header": self.header})
+
     def flat_first(self) -> IllustMessageModel:
-        return self.messages[0].copy(update={"header": self.header})
+        for x in self.flat():
+            return x
