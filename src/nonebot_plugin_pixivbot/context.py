1e146d50d40e51f6cc591c9b66d2f80232951720
huangwenlong06 <huangwenlong06@meituan.com>
2022-08-05 14:32:35 +0800

    修复py3.10以下版本没有get_annotations的问题

--- src/nonebot_plugin_pixivbot/context.py
+++ src/nonebot_plugin_pixivbot/context.py
@@ -1,8 +1,86 @@
+import functools
 import inspect
+import sys
+import types
 from threading import Lock
-from typing import TypeVar, Type, Callable
+from typing import TypeVar, Type, Callable, Dict, Any
 
 from nonebot.log import logger
+
+
+# copied from inspect.py (py3.10)
+def get_annotations(obj, *, globals=None, locals=None, eval_str=False) -> Dict[str, Any]:
+    if sys.version_info >= (3, 10):
+        return inspect.get_annotations(obj, globals=globals, locals=locals, eval_str=eval_str)
+
+    if isinstance(obj, type):
+        # class
+        obj_dict = getattr(obj, '__dict__', None)
+        if obj_dict and hasattr(obj_dict, 'get'):
+            ann = obj_dict.get('__annotations__', None)
+            if isinstance(ann, types.GetSetDescriptorType):
+                ann = None
+        else:
+            ann = None
+
+        obj_globals = None
+        module_name = getattr(obj, '__module__', None)
+        if module_name:
+            module = sys.modules.get(module_name, None)
+            if module:
+                obj_globals = getattr(module, '__dict__', None)
+        obj_locals = dict(vars(obj))
+        unwrap = obj
+    elif isinstance(obj, types.ModuleType):
+        # module
+        ann = getattr(obj, '__annotations__', None)
+        obj_globals = getattr(obj, '__dict__')
+        obj_locals = None
+        unwrap = None
+    elif callable(obj):
+        # this includes types.Function, types.BuiltinFunctionType,
+        # types.BuiltinMethodType, functools.partial, functools.singledispatch,
+        # "class funclike" from Lib/test/test_inspect... on and on it goes.
+        ann = getattr(obj, '__annotations__', None)
+        obj_globals = getattr(obj, '__globals__', None)
+        obj_locals = None
+        unwrap = obj
+    else:
+        raise TypeError(f"{obj!r} is not a module, class, or callable.")
+
+    if ann is None:
+        return {}
+
+    if not isinstance(ann, dict):
+        raise ValueError(f"{obj!r}.__annotations__ is neither a dict nor None")
+
+    if not ann:
+        return {}
+
+    if not eval_str:
+        return dict(ann)
+
+    if unwrap is not None:
+        while True:
+            if hasattr(unwrap, '__wrapped__'):
+                unwrap = unwrap.__wrapped__
+                continue
+            if isinstance(unwrap, functools.partial):
+                unwrap = unwrap.func
+                continue
+            break
+        if hasattr(unwrap, "__globals__"):
+            obj_globals = unwrap.__globals__
+
+    if globals is None:
+        globals = obj_globals
+    if locals is None:
+        locals = obj_locals
+
+    return_value = {key: value if not isinstance(value, str) else eval(value, globals, locals)
+                    for key, value in ann.items()}
+    return return_value
+
 
 T = TypeVar("T")
 
@@ -123,7 +201,7 @@ class Context:
         old_getattr = getattr(cls, "__getattr__", None)
 
         def __getattr__(obj: T, name: str):
-            ann = inspect.get_annotations(cls, eval_str=True)
+            ann = get_annotations(cls, eval_str=True)
             if name in ann and ann[name] in self:
                 return self[ann[name]]
 
