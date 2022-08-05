5da631e5adc02364421733c0a456630c54bbfcde
ssttkkl <huang.wen.long@hotmail.com>
2022-08-05 21:35:51 +0800

    添加等待时间超过3s发送加载提示的功能

--- src/nonebot_plugin_pixivbot/handler/interceptor/interceptor.py
+++ src/nonebot_plugin_pixivbot/handler/interceptor/interceptor.py
@@ -2,13 +2,29 @@ from abc import ABC, abstractmethod
 from functools import wraps
 from typing import Callable, TypeVar
 
+from nonebot_plugin_pixivbot import context
 from nonebot_plugin_pixivbot.protocol_dep.post_dest import PostDestination
+from nonebot_plugin_pixivbot.protocol_dep.postman import PostmanManager
 
 UID = TypeVar("UID")
 GID = TypeVar("GID")
 
 
+@context.inject
 class Interceptor(ABC):
+    postman_manager: PostmanManager
+
+    async def post_plain_text(self, message: str,
+                              post_dest: PostDestination):
+        await self.postman_manager.send_plain_text(message, post_dest=post_dest)
+
+    @abstractmethod
+    async def intercept(self, wrapped_func: Callable, *,
+                        post_dest: PostDestination[UID, GID],
+                        silently: bool,
+                        **kwargs):
+        raise NotImplementedError()
+
     def __call__(self, wrapped: Callable):
         @wraps(wrapped)
         async def wrapper(post_dest: PostDestination[UID, GID],
@@ -17,10 +33,3 @@ class Interceptor(ABC):
             await self.intercept(wrapped, post_dest=post_dest, silently=silently, **kwargs)
 
         return wrapper
-
-    @abstractmethod
-    async def intercept(self, wrapped_func: Callable, *,
-                        post_dest: PostDestination[UID, GID],
-                        silently: bool,
-                        **kwargs):
-        raise NotImplementedError()
