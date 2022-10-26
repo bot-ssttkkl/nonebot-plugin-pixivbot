bd80c245ea9b9fdc1389ff2c0a0f5a9409069877
ssttkkl <huang.wen.long@hotmail.com>
2022-10-26 20:48:32 +0800

    添加插件元数据

--- src/nonebot_plugin_pixivbot/__init__.py
+++ src/nonebot_plugin_pixivbot/__init__.py
@@ -6,6 +6,33 @@ nonebot-plugin-pixivbot
 @GitHub         : https://github.com/ssttkkl/nonebot-plugin-pixivbot
 """
 
+help_text = """
+常规语句：
+- 看看榜<范围>：查看pixiv榜单
+- 来张图：从推荐插画随机抽选一张插画
+- 来张<关键字>图：搜索关键字，从搜索结果随机抽选一张插画
+- 来张<用户>老师的图：搜索画师，从该画师的插画列表里随机抽选一张插画
+- 看看图<插画ID>：查看id为<插画ID>的插画
+- 来张私家车：从书签中随机抽选一张插画
+- 还要：重复上一次请求
+- 不够色：获取上一张插画的相关推荐
+
+命令语句：
+- /pixivbot help：查看本帮助
+- /pixivbot bind：绑定Pixiv账号
+
+更多功能：参见https://github.com/ssttkkl/nonebot-plugin-pixivbot
+""".strip()
+
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name='PixivBot',
+    description='发送随机Pixiv插画、画师更新推送、定时订阅推送……',
+    usage=help_text,
+    extra={'version': '1.4.0'}
+)
+
 # ================= provide beans =================
 from importlib import import_module
 
--- src/nonebot_plugin_pixivbot/handler/command/help.py
+++ src/nonebot_plugin_pixivbot/handler/command/help.py
@@ -1,5 +1,6 @@
 from typing import Union, Awaitable, TypeVar, Sequence
 
+from nonebot_plugin_pixivbot import help_text
 from nonebot_plugin_pixivbot.global_context import context
 from nonebot_plugin_pixivbot.protocol_dep.post_dest import PostDestination
 from .command import SubCommandHandler, CommandHandler
@@ -10,23 +11,6 @@ GID = TypeVar("GID")
 
 @context.require(CommandHandler).sub_command("help")
 class HelpHandler(SubCommandHandler):
-    help_text = """常规语句：
-- 看看榜<范围>：查看pixiv榜单
-- 来张图：从推荐插画随机抽选一张插画
-- 来张<关键字>图：搜索关键字，从搜索结果随机抽选一张插画
-- 来张<用户>老师的图：搜索画师，从该画师的插画列表里随机抽选一张插画
-- 看看图<插画ID>：查看id为<插画ID>的插画
-- 来张私家车：从书签中随机抽选一张插画
-- 还要：重复上一次请求
-- 不够色：获取上一张插画的相关推荐
-
-命令语句：
-- /pixivbot help：查看本帮助
-- /pixivbot bind：绑定Pixiv账号
-
-更多功能：参见https://github.com/ssttkkl/nonebot-plugin-pixivbot
-"""
-
     @classmethod
     def type(cls) -> str:
         return "help"
@@ -40,4 +24,4 @@ class HelpHandler(SubCommandHandler):
 
     async def actual_handle(self, *, post_dest: PostDestination[UID, GID],
                             silently: bool = False):
-        await self.post_plain_text(self.help_text, post_dest=post_dest)
+        await self.post_plain_text(help_text, post_dest=post_dest)
