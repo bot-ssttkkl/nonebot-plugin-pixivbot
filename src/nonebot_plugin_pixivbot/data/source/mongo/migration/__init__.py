cfcd2d7b44a11f4e3b43ca52ef79977c52eab704
ssttkkl <huang.wen.long@hotmail.com>
2022-11-01 23:09:02 +0800

    MongoDB添加4到5的迁移

--- src/nonebot_plugin_pixivbot/data/source/mongo/migration/__init__.py
+++ src/nonebot_plugin_pixivbot/data/source/mongo/migration/__init__.py
@@ -1,6 +1,7 @@
 from . import mongo_v1_to_v2
 from . import mongo_v2_to_v3
 from . import mongo_v3_to_v4
+from . import mongo_v4_to_v5
 from .mongo_migration_manager import MongoMigrationManager
 
 __all__ = ("MongoMigrationManager",)
