1f875ff1bee342db0cf87f0bd549582cbf016d93
ssttkkl <huang.wen.long@hotmail.com>
2022-11-03 13:37:32 +0800

    不填入pixiv_mongo_conn_url时默认为sqlite数据源

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,7 +3,5 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def conf_env(monkeypatch):
-    monkeypatch.setenv("PIXIV_MONGO_CONN_URL", "123")
-    monkeypatch.setenv("PIXIV_MONGO_DATABASE_NAME", "123")
     monkeypatch.setenv("PIXIV_REFRESH_TOKEN", "123")
     monkeypatch.setenv("SUPERUSERS", "[\"test:123456\"]")
