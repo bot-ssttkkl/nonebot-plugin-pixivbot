15d3e7375c4280b8b9a25b4a46e00e471901ebc5
ssttkkl <huang.wen.long@hotmail.com>
2022-11-03 13:15:26 +0800

    使用cachetools做recorder

--- src/nonebot_plugin_pixivbot/handler/recorder.py
+++ src/nonebot_plugin_pixivbot/handler/recorder.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-import time
-from collections import OrderedDict
 from typing import Optional
 
-from nonebot import logger
+from cachetools import TTLCache
 
 from nonebot_plugin_pixivbot.config import Config
 from nonebot_plugin_pixivbot.context import Inject
@@ -16,16 +14,10 @@ from .pkg_context import context
 
 
 class Req:
-    def __init__(self, func,
-                 *args, **kwargs):
-        self.timestamp = 0
+    def __init__(self, func, *args, **kwargs):
         self.func = func
         self.args = args
         self.kwargs = kwargs
-        self.refresh()
-
-    def refresh(self):
-        self.timestamp = time.time()
 
     def __call__(self, **kwargs):
         actual_kwargs = self.kwargs.copy()
@@ -37,12 +29,7 @@ class Req:
 
 class Resp:
     def __init__(self, illust_id: int):
-        self.timestamp = 0
         self.illust_id = illust_id
-        self.refresh()
-
-    def refresh(self):
-        self.timestamp = time.time()
 
 
 @context.inject
@@ -52,8 +39,10 @@ class Recorder:
 
     def __init__(self, max_req_size: int = 65535,
                  max_resp_size: int = 65535):
-        self._reqs = OrderedDict[PostIdentifier[T_UID, T_GID], Req]()
-        self._resps = OrderedDict[PostIdentifier[T_UID, T_GID], Resp]()
+        self._reqs = TTLCache[PostIdentifier[T_UID, T_GID], Req](maxsize=max_req_size,
+                                                                 ttl=self.conf.pixiv_query_expires_in)
+        self._resps = TTLCache[PostIdentifier[T_UID, T_GID], Resp](maxsize=max_resp_size,
+                                                                   ttl=self.conf.pixiv_query_expires_in)
 
         self.max_req_size = max_req_size
         self.max_resp_size = max_resp_size
@@ -65,32 +54,12 @@ class Recorder:
         else:
             return None
 
-    def _collate_req(self, ensure_size_for_put: bool):
-        now = time.time()
-        while len(self._reqs) > 0:
-            key, req = next(iter(self._reqs.items()))
-            if now - req.timestamp > self.conf.pixiv_query_expires_in:
-                self._reqs.popitem(last=False)
-                logger.info(f"popped expired req: ({key})")
-            else:
-                break
-
-        if ensure_size_for_put and len(self._reqs) == self.max_req_size:
-            key, req = self._reqs.popitem(last=False)
-            logger.info(f"popped first req: ({key})")
-
     def record_req(self, record: Req, key: PostIdentifier[T_UID, T_GID]):
-        self._collate_req(True)
-        if key in self._reqs:
-            self._reqs.move_to_end(key)
         self._reqs[key] = record
 
     def get_req(self, key: PostIdentifier[T_UID, T_GID]) -> Optional[Req]:
-        self._collate_req(False)
         if key in self._reqs:
             req = self._reqs[key]
-            req.refresh()
-            self._reqs.move_to_end(key)
             return req
         else:
             key = self._key_fallback(key)
@@ -99,28 +68,10 @@ class Recorder:
             else:
                 return None
 
-    def _collate_resp(self, ensure_size_for_put: bool):
-        now = time.time()
-        while len(self._resps) > 0:
-            key, resp = next(iter(self._resps.items()))
-            if now - resp.timestamp > self.conf.pixiv_query_expires_in:
-                self._resps.popitem(last=False)
-                logger.info(f"popped expired resp illust: ({key})")
-            else:
-                break
-
-        if ensure_size_for_put and len(self._resps) == self.max_resp_size:
-            key, resp = self._resps.popitem(last=False)
-            logger.info(f"popped first resp illust: ({key})")
-
     def record_resp(self, illust_id: int, key: PostIdentifier[T_UID, T_GID]):
-        self._collate_resp(True)
-        if key in self._resps:
-            self._resps.move_to_end(key)
         self._resps[key] = Resp(illust_id)
 
     def get_resp(self, key: PostIdentifier[T_UID, T_GID]) -> Optional[int]:
-        self._collate_resp(False)
         if key in self._resps:
             rec = self._resps[key]
             return rec.illust_id
