f82aad8efde538d96ec8f6bdbfba898bbbc74ea7
ssttkkl <huang.wen.long@hotmail.com>
2022-12-09 21:04:54 +0800

    修复PixivRepoMetadata的update默认值不正确的问题

--- src/nonebot_plugin_pixivbot/data/pixiv_repo/models.py
+++ src/nonebot_plugin_pixivbot/data/pixiv_repo/models.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timezone
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class PixivRepoMetadata(BaseModel):
-    update_time: datetime = datetime.now(timezone.utc)
+    update_time: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     pages: Optional[int]
     next_qs: Optional[dict]
 
