dbc183a3acd832e8a5c8c9e06b96dcddc3033f6c
ssttkkl <huang.wen.long@hotmail.com>
2023-08-30 00:11:12 +0800

    将部分通用逻辑抽至ssttkkl-nonebot-utils

--- src/nonebot_plugin_pixivbot/__init__.py
+++ src/nonebot_plugin_pixivbot/__init__.py
@@ -10,15 +10,6 @@ from nonebot.plugin import PluginMetadata
 from .config import Config
 from .usage import usage
 
-__plugin_meta__ = PluginMetadata(
-    name='PixivBot',
-    description='发送随机Pixiv插画、画师更新推送、定时订阅推送……',
-    usage=usage,
-    type="application",
-    homepage="https://github.com/ssttkkl/nonebot-plugin-pixivbot",
-    config=Config,
-)
-
 # =========== require dependency ============
 from nonebot import require
 
@@ -26,6 +17,19 @@ require("nonebot_plugin_apscheduler")
 require("nonebot_plugin_access_control")
 require("nonebot_plugin_session")
 require("nonebot_plugin_saa")
+
+# =========== plugin meta ============
+from nonebot_plugin_saa import __plugin_meta__ as saa_meta
+
+__plugin_meta__ = PluginMetadata(
+    name='PixivBot',
+    description='发送随机Pixiv插画、画师更新推送、定时订阅推送……',
+    usage=usage,
+    type="application",
+    homepage="https://github.com/bot-ssttkkl/nonebot-plugin-pixivbot",
+    config=Config,
+    supported_adapters=saa_meta.supported_adapters
+)
 
 # =========== register handler & service ============
 from . import service
--- src/nonebot_plugin_pixivbot/platform/__init__.py
+++
@@ -1,7 +0,0 @@
-from .available import register as register_available
-from .func_manager import FuncManagerFactory
-
-platform_func = FuncManagerFactory()
-register_available(platform_func)
-
-__all__ = ("platform_func",)
